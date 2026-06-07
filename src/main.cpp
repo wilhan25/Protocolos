3b061b20209d335fffb464e41f9fc9c08fc47fe7
Wilhan Almeida <wilhan.amello@gmail.com>
2026-06-07 05:28:49 -0300

    pratica do tcp

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,69 @@
 #include <Arduino.h>
-#include <Wire.h>
+#include <WiFi.h>
 
-//pinos do I2C
-#define pin_SDA 23
-#define pin_SCL 21
-#define mpu_addr  0x68 // endereço I2C do MPU6050
-#define reg_pwr_mgmt_1 0x6B //registrador de energia do 6050
-#define reg_accel_xout_h  0x3B //registrador que começa os dados do 6050
+const char* SSID = "seu wifi";
+const char* senha = "sua senha do wifi";
 
-//controlador do semaforo (chave do banheiro)
-SemaphoreHandle_t xMutexI2C = NULL;
+WiFiServer  servidorTCP(8080);
 
-void Acordar6050(){
-  Wire.beginTransmission(mpu_addr);
-  Wire.write(reg_pwr_mgmt_1);
-  Wire.write(0x00);
-  Wire.endTransmission();
-  Serial.println("[6050] SENSOR ACORDADO!");
-}
+void TaskConexao(void *pv){
+  WiFi.begin(SSID,senha);
+  Serial.println("[WIFI]  Conectando");
 
-void TaskLeituraAcelerometro(void* pv){
   while (1)
   {
-    /* code */
-    if(xSemaphoreTake(xMutexI2C,pdMS_TO_TICKS(100))==pdTRUE){
-      Wire.beginTransmission(mpu_addr); //abre conexão com o 6050
-      Wire.write(reg_accel_xout_h);
-      Wire.endTransmission(false);// O 'false' envia um RESTART no barramento, mantendo a linha presa
-
-      Wire.requestFrom(mpu_addr,6);
-
-      if(Wire.available() == 6){
-        int16_t rawX = (Wire.read()<<8 | Wire.read()); //a informação tem 16bit e o registrador le de 8, então se le 8, desloca pra esquerda e le os proximos 8 bits
-        int16_t rawY = (Wire.read()<<8 | Wire.read());
-        int16_t rawZ = (Wire.read()<<8 | Wire.read());
-
-        Serial.printf("[6050] X: %6d | Y: %6d | Z: %6d\n", rawX,rawY,rawZ);
-      }
-
-      xSemaphoreGive(xMutexI2C);
+    if(WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      vTaskDelay(pdMS_TO_TICKS(500));
     }
     else{
-      Serial.println("[AVISO] task não consegui acesso a chave do banheiro");
+      Serial.println("\n[WIFI] Conectado com Sucesso!");
+      Serial.print("[WIFI] O IP do esp32 é: ");
+      Serial.println(WiFi.localIP());
+      servidorTCP.begin();
+      vTaskDelay(pdMS_TO_TICKS(10000));
     }
-    vTaskDelay(pdMS_TO_TICKS(500));
   }  
 }
 
+void TaskEscutaTCP(void *pv){
+  while (1)
+  {
+    WiFiClient cliente = servidorTCP.available();
+
+    if(cliente){
+      Serial.println("[TCP] Novo cliente conectado!");
+
+      cliente.println("Bem-Vindo so Servidor do ESP32");
+      cliente.println("Digite uma mensgem e aperte ENTER:");
+
+      while (cliente.connected())
+      {
+        /* code */
+        if(cliente.available()){
+          String mensagemRecebida = cliente.readStringUntil('\n');
+
+          Serial.print("[CLIENTE]:");
+          Serial.println(mensagemRecebida);
+
+          cliente.print("ESP32 recebeu essa mensagem:");
+          cliente.println(mensagemRecebida);
+        }
+        vTaskDelay(pdMS_TO_TICKS(10));
+      }
+      cliente.stop();
+      Serial.println("[TCP] Cliente desconectou");
+    }
+    vTaskDelay(pdMS_TO_TICKS(100));
+  }  
+}
 
 void setup() { 
   Serial.begin(115200);
   vTaskDelay(pdMS_TO_TICKS(5000));
 
-  Serial.println("--- Inicializando Leitura MPU6050 via FreeRTOS ---");
-
-  Wire.begin(pin_SDA,pin_SCL);
-
-  xMutexI2C = xSemaphoreCreateMutex();
-  if(xMutexI2C != NULL){
-    Acordar6050();
-    xTaskCreate(TaskLeituraAcelerometro,"6050", 3072, NULL, 2, NULL);
-  }else {
-    Serial.println("Erro ao criar o Mutex!");
-  }
+  xTaskCreate(TaskConexao,"conexao", 4096,NULL,1,NULL);
+  xTaskCreate(TaskEscutaTCP,"tcp", 4096,NULL,2,NULL);
 }
 
 void loop() {
